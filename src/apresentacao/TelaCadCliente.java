d9de274b59f1f86368baa8faa2406c457942e9cc
Luis Faria <luisfaria.go@gmail.com>
2018-11-05 21:00:27 -0200

    validação cpf

--- src/apresentacao/TelaCadCliente.java
+++ src/apresentacao/TelaCadCliente.java
@@ -9,6 +9,7 @@ import entidade.ClientePF;
 import javax.swing.JDesktopPane;
 import javax.swing.JOptionPane;
 import negocio.NClientePF;
+import util.ValidaCPF;
 
 /**
  *
@@ -31,7 +32,7 @@ public class TelaCadCliente extends javax.swing.JInternalFrame {
     }
 
     private void limpar() {
-        
+
         jTextFieldNome.setText("");
         jTextFieldTelefone.setText("");
         jTextFieldEndereco.setText("");
@@ -52,7 +53,6 @@ public class TelaCadCliente extends javax.swing.JInternalFrame {
         jPanel1 = new javax.swing.JPanel();
         jLabel1 = new javax.swing.JLabel();
         jTextFieldNome = new javax.swing.JTextField();
-        jTextFieldCPFCNPJ = new javax.swing.JTextField();
         jLabel2 = new javax.swing.JLabel();
         jTextFieldTelefone = new javax.swing.JTextField();
         jLabel3 = new javax.swing.JLabel();
@@ -62,6 +62,7 @@ public class TelaCadCliente extends javax.swing.JInternalFrame {
         jLabel5 = new javax.swing.JLabel();
         jLabel8 = new javax.swing.JLabel();
         jTextFieldCodigo = new javax.swing.JTextField();
+        jTextFieldCPFCNPJ = new javax.swing.JFormattedTextField();
         btnVoltar = new javax.swing.JButton();
         btnLimpar = new javax.swing.JButton();
         btnSalvar = new javax.swing.JButton();
@@ -91,6 +92,12 @@ public class TelaCadCliente extends javax.swing.JInternalFrame {
 
         jTextFieldCodigo.setEditable(false);
 
+        try {
+            jTextFieldCPFCNPJ.setFormatterFactory(new javax.swing.text.DefaultFormatterFactory(new javax.swing.text.MaskFormatter("###.###.###-##")));
+        } catch (java.text.ParseException ex) {
+            ex.printStackTrace();
+        }
+
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
@@ -101,7 +108,7 @@ public class TelaCadCliente extends javax.swing.JInternalFrame {
                     .addGroup(jPanel1Layout.createSequentialGroup()
                         .addComponent(jLabel2)
                         .addGap(18, 18, 18)
-                        .addComponent(jTextFieldCPFCNPJ, javax.swing.GroupLayout.PREFERRED_SIZE, 333, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addComponent(jTextFieldCPFCNPJ))
                     .addGroup(jPanel1Layout.createSequentialGroup()
                         .addComponent(jLabel3)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
@@ -142,7 +149,7 @@ public class TelaCadCliente extends javax.swing.JInternalFrame {
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel2)
                     .addComponent(jTextFieldCPFCNPJ, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addGap(18, 18, 18)
+                .addGap(20, 20, 20)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel3)
                     .addComponent(jTextFieldTelefone, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
@@ -227,6 +234,7 @@ public class TelaCadCliente extends javax.swing.JInternalFrame {
 
     private void btnSalvarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnSalvarActionPerformed
         try {
+            ClientePF clientePF = new ClientePF();
 
             if (jTextFieldNome.getText().isEmpty()) {
                 throw new Exception("E necessário preencher o nome.");
@@ -234,6 +242,15 @@ public class TelaCadCliente extends javax.swing.JInternalFrame {
             if (jTextFieldCPFCNPJ.getText().isEmpty()) {
                 throw new Exception("E necessário preencher o CPF.");
             }
+            String cpf = jTextFieldCPFCNPJ.getText();
+            cpf = cpf.replace(".", "");
+            cpf = cpf.replace("-", "");
+            clientePF.setCpf(cpf);
+            if (ValidaCPF.isCPF(cpf) == true) {
+            } else {
+                throw new Exception("CPF inválido! Digite novamente.");
+            }
+
             if (jTextFieldTelefone.getText().isEmpty()) {
                 throw new Exception("E necessário preencher o telefone.");
             }
@@ -243,14 +260,12 @@ public class TelaCadCliente extends javax.swing.JInternalFrame {
             if (jTextFieldEmail.getText().isEmpty()) {
                 throw new Exception("E necessário preencher o email.");
             }
-            ClientePF clientePF = new ClientePF();
 
             if (!jTextFieldCodigo.getText().isEmpty()) {
                 clientePF.setIdentificador(Integer.parseInt(jTextFieldCodigo.getText()));
             }
 
             clientePF.setNome(jTextFieldNome.getText());
-            clientePF.setCpf(jTextFieldCPFCNPJ.getText());
             clientePF.setTelefone(jTextFieldTelefone.getText());
             clientePF.setEndereco(jTextFieldEndereco.getText());
             clientePF.setEmail(jTextFieldEmail.getText());
@@ -264,7 +279,7 @@ public class TelaCadCliente extends javax.swing.JInternalFrame {
 
         } catch (Exception e) {
             JOptionPane.showMessageDialog(null, e.getMessage());
-             e.printStackTrace();
+            e.printStackTrace();
         }
     }//GEN-LAST:event_btnSalvarActionPerformed
 
@@ -283,7 +298,7 @@ public class TelaCadCliente extends javax.swing.JInternalFrame {
     private javax.swing.JLabel jLabel5;
     private javax.swing.JLabel jLabel8;
     private javax.swing.JPanel jPanel1;
-    private javax.swing.JTextField jTextFieldCPFCNPJ;
+    private javax.swing.JFormattedTextField jTextFieldCPFCNPJ;
     private javax.swing.JTextField jTextFieldCodigo;
     private javax.swing.JTextField jTextFieldEmail;
     private javax.swing.JTextField jTextFieldEndereco;
